bc26632d2701139d0549d08538ea0e1d449e041b
Divyanshu Singh <divic309>
2021-01-29 23:15:10 +0530

    .

--- whatsapp/src/Sidebar.js
+++ whatsapp/src/Sidebar.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 import "./Sidebar.css";
 
-
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
             <Avatar />
-            <div className="sidebar__header__right">
+            <div className="sidebar__header-right">
+                <h1>Hello</h1>
                 <IconButton>
                 <DonutLargeIcon />
                 </IconButton>
